1090466afbadb79744404f76c92badf98ea81808
jaisat <jaiswalsatyam211@gmail.com>
2021-06-30 15:42:43 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AppClass from './AppClass'
+
+function App() {
+  return (
+    <div className="App">
+      <AppClass/>
+    </div>
+  );
+}
+
+export default App;
